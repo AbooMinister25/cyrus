e1b3a63fc4eb6da4e55e9c99ed77b20e604b8c5f
AbooMinister25 <aboominister@gmail.com>
2023-09-21 19:04:30 -0400

    Satisfy lints

--- src/cyrus/__init__.py
+++ src/cyrus/__init__.py
@@ -3,10 +3,14 @@
 __VERSION__ = "0.1.0"
 
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Static, Footer
+from textual.widgets import Footer, Header, Static
 
 
 class Cyrus(App[None]):
+    """A TUI based Markdown editor."""
+
+    BINDINGS = [("q", "quit", "Quit")]
+
     def compose(self) -> ComposeResult:
         yield Header()
         yield Static("Hello, World")
